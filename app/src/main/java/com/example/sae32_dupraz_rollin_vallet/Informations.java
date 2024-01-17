1f971d5f8b9315fd0909493e42faf36314b4b216
nawgrimm <nawgrimm@gmail.com>
2024-01-17 16:04:34 +0100

    jvj

--- app/src/main/java/com/example/sae32_dupraz_rollin_vallet/Informations.java
+++ app/src/main/java/com/example/sae32_dupraz_rollin_vallet/Informations.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Informations extends AppCompatActivity {
+public class  Informations extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
