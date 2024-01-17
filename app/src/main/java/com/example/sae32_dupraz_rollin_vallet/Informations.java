f11f5e15299bba11eaf61f7d033efb8b1b0a0043
max51v2 <valetmaxx@gmail.com>
2024-01-17 16:23:42 +0100

    a

--- app/src/main/java/com/example/sae32_dupraz_rollin_vallet/Informations.java
+++ app/src/main/java/com/example/sae32_dupraz_rollin_vallet/Informations.java
@@ -2,7 +2,9 @@ package com.example.sae32_dupraz_rollin_vallet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class  Informations extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class  Informations extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_informations);
+
+        View InformationsBannerButtonHome = findViewById(R.id.InformationsBannerButtonHome);
+
+        //Changement d'activité (IP)
+        InformationsBannerButtonHome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(Informations.this, MainActivity.class));
+            }
+        });
     }
 }
