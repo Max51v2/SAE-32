d480478b7a4c9e748f12f10529d4e3e7bcb51c6b
max51v2 <valetmaxx@gmail.com>
2024-01-22 11:58:25 +0100

    a

--- app/src/main/java/com/example/sae32_dupraz_rollin_vallet/WIFI.java
+++ app/src/main/java/com/example/sae32_dupraz_rollin_vallet/WIFI.java
@@ -3,8 +3,15 @@ package com.example.sae32_dupraz_rollin_vallet;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 public class WIFI extends AppCompatActivity {
 
@@ -16,6 +23,8 @@ public class WIFI extends AppCompatActivity {
 
         //_____________Variables/Objets_______________
         View WIFIBannerButtonHome = findViewById(R.id.WIFIBannerButtonHome);
+        View WIFITextIP = findViewById(R.id.WIFITextIP);
+        final EditText WIFITextIP2 =  (EditText) findViewById(R.id.WIFITextIP2);
         //____________________________________________
 
 
@@ -25,5 +34,9 @@ public class WIFI extends AppCompatActivity {
                 startActivity(new Intent(WIFI.this, MainActivity.class).setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
             }
         });
+
+
+        //Infos
+
     }
 }
